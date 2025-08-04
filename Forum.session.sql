5bc9bd19dd8e1154c6b162304802bd4fdf35a4ee
Robert Johnston <anaerin@gmail.com>
2025-08-03 23:10:47 -0600

    Start downloading assets from the wayback machine (and maybe missing posts too - Untested)

--- Forum.session.sql
+++
@@ -1,4 +0,0 @@
-SELECT Users.id, Users.name, Users.avatar, Users.avatarHeight, Users.avatarWidth, Users.location, Users.rank, ? FROM Users.firstVideo, COUNT(Posts.id) AS PostCount FROM Users
-INNER JOIN Posts
-ON Posts.UserId = Users.id
-GROUP BY Posts.UserId
